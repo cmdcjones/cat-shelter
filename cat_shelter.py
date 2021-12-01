366c7ceab0d9cf5a2308e1e507b6be60b0122d53
Dominique Chin-Jones <domecj13@gmail.com>
2021-12-01 18:26:27 -0500

    added extra comments

--- cat_shelter.py
+++ cat_shelter.py
@@ -1,4 +1,4 @@
-# Cat Shelter project
+# Cat Shelter project - Be in charge of a cat shelter! Admit, feed, bathe, and put cats up for adoption!
 # Concepts used: imports, classes, functions, class attributes, methods, user input, lists, loops, conditionals, standard modules (random)
 # ----------------------------------------------------------------------------------------------------------------------------------------
 # TODO:
